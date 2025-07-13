95bc55a7abbccab9bd9f24639f1440374a79d161
Shhrreyy <shriyanshyadav2006@gmail.com>
2025-07-13 11:07:04 +0530

    Create 03_pos_neg_integers.cpp

---
+++ 03_pos_neg_integers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num;
+cout<<"Enter any Number:";
+cin<<num;
+
+if(num<0){
+cout<<"The Number is -ve"<<endl;
+}
+
+else if(num>0){
+cout<<"The Number is +ve"<<endl;
+}
+
+else{
+cout<<"The Number is Zero"<<endl;
+}
+
+return 0;
+}
