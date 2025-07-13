05ae5602510e282a05f09cb5da982210ffe02a93
Shhrreyy <shriyanshyadav2006@gmail.com>
2025-07-13 10:56:36 +0530

    Update and rename 02_ar_operators to 02_ar_operators.cpp

---
+++ 02_ar_operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int num1,num2;
+cout<<"Enter first number:";
+cin>> num1;
+
+cout<<"Enter second number:";
+cinn>> num2;
+
+int sum = num1 + num2;
+cout<<"Sum:"<< sum<< endl;
+
+int sub = num1-num2;
+cout<<"Sub:"<<sub<< endl;
+
+return o;
+}
