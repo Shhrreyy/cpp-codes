440352047e4eff181d2bcfd9f1daaace2ce8fc7d
Shreyy <shriyanshyadav2006@gmail.com>
2025-07-25 10:26:42 +0530

    Create alphabetfloyd_25jul.cpp

---
+++ alphabetfloyd_25jul.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n=5;
+    char c = 'A';
+    for(int i=1;i<=n;i++){
+        
+        for(char k=1;k<=i;k++){
+            cout<<c<<" ";
+            c++;
+            
+        }
+        cout<<endl;
+    }
+
+    }
